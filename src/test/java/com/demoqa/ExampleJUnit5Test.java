07f04d305012d86b0e90c26f04efe621174ab8c2
Miminosha <anna.memikova@yandex.ru>
2022-07-31 23:16:20 +0300

    first

---
+++ src/test/java/com/demoqa/ExampleJUnit5Test.java
@@ -0,0 +1,39 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.*;
+
+import java.lang.module.Configuration;
+
+public class ExampleJUnit5Test {
+
+    @BeforeAll
+    static void configure(){
+        System.out.println("    This is the @BeforeAll");
+    }
+
+    @AfterAll
+    static void tearDown(){
+        System.out.println("    This is the @AfterAll");
+    }
+
+    @BeforeEach
+    void beforeTest(){
+        System.out.println("    This is the @BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("    This is the @AfterEach");
+    }
+
+    @Test
+    void firstTest0(){
+        Assertions.assertTrue(3>2);
+        System.out.println("    This is the @Test0");
+    }
+
+    @Test
+    void firstTest1(){
+        System.out.println("    This is the @Test1");
+    }
+}
